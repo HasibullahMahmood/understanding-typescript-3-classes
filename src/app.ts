81a7d9bc76f7fe4a909f728419847f93ef882fa6
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-01-07 08:15:17 +0300

    5.20 extending interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 // used to define structure of an object
 // also used as type
-interface Greetable {
-	name: string;
-	email: string;
 
+interface Named {
+	readonly name: string;
+}
+
+interface Greetable extends Named {
+	email: string;
 	describe(phrase: string): void;
 }
 
@@ -15,5 +18,7 @@ class Person implements Greetable {
 	}
 }
 
-const user1 = new Person('Hasibullah', 'hasibullah@test.com', 25);
+let user1: Greetable;
+user1 = new Person('Hasibullah', 'hasibullah@test.com', 25);
+// user1.name = 'Manu';
 user1.describe("Hi-I'm ");
